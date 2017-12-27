26c059d3e831b393e125ec2802da26a9cce7f477
jonkrause <jonathonkrause@gmail.com>
2017-12-27 16:46:53 -0500

    another stupid commit

--- app.js
+++ app.js
@@ -1,106 +1,108 @@
-var start = "wow"
+var start = new Date().getTime()
 console.log(start)
-// var arr01 = ['kiss', 'east', 'garbage', 'west']
-// var arr02 = ['army', 'practice', 'script', 'answer']
-// var arr03 = ['similar', 'jelly', 'crush', 'fiscal']
-// var arr04 = ['decade', 'rifle', 'cousin', 'wool']
-// var arr05 = ['second', 'helmet', 'decade', 'helmet']
-// var arr06 = ['polar', 'decide', 'second', 'helmet']
-// var arr07 = ['cousin', 'buzz', 'popular', 'music']
-// var arr08 = ['issue', 'luggage', 'enact', 'jelly']
-// var arr09 = ['cycle', 'transfer', 'army', 'wool']
-// var arr10 = ['script', 'damage', 'script', 'thunder']
-// var arr11 = ['enact', 'wool', 'blind', 'rifle']
-// var arr12 = ['garbage', 'chest', 'script', 'wool']
-// var arr13 = ['popular', 'fiscal', 'blind', 'hazard']
-// var arr14 = ['electric', 'spoon', 'lab', 'course']
-// var arr15 = ['crush', 'banner', 'decade', 'wool']
-// var arr16 = ['search', 'answer', 'disease', 'transfer']
-// var arr17 = ['disease', 'chef', 'lab', 'east']
-// var arr18 = ['blind', 'hazard', 'lab', 'banner']
-// var arr19 = ['shrug', 'music', 'garbage', 'chest']
-// var arr20 = ['then', 'swing', 'issue', 'disagree']
-// var arr21 = ['lab', 'course', 'disease', 'wool']
-// var arr22 = ['expand', 'disagree', 'disease', 'hazard']
-// var arr23 = ['advice', 'thunder', 'electric', 'helmet']
-// var arr24 = ['help', 'west', 'crush', 'music']
-// var output = []
+var arr01 = ['kiss', 'east', 'garbage', 'west']
+var arr02 = ['army', 'practice', 'script', 'answer']
+var arr03 = ['similar', 'jelly', 'crush', 'fiscal']
+var arr04 = ['decade', 'rifle', 'cousin', 'wool']
+var arr05 = ['second', 'helmet', 'decade', 'helmet']
+var arr06 = ['polar', 'decide', 'second', 'helmet']
+var arr07 = ['cousin', 'buzz', 'popular', 'music']
+var arr08 = ['issue', 'luggage', 'enact', 'jelly']
+var arr09 = ['cycle', 'transfer', 'army', 'wool']
+var arr10 = ['script', 'damage', 'script', 'thunder']
+var arr11 = ['enact', 'wool', 'blind', 'rifle']
+var arr12 = ['garbage', 'chest', 'script', 'wool']
+var arr13 = ['popular', 'fiscal', 'blind', 'hazard']
+var arr14 = ['electric', 'spoon', 'lab', 'course']
+var arr15 = ['crush', 'banner', 'decade', 'wool']
+var arr16 = ['search', 'answer', 'disease', 'transfer']
+var arr17 = ['disease', 'chef', 'lab', 'east']
+var arr18 = ['blind', 'hazard', 'lab', 'banner']
+var arr19 = ['shrug', 'music', 'garbage', 'chest']
+var arr20 = ['then', 'swing', 'issue', 'disagree']
+var arr21 = ['lab', 'course', 'disease', 'wool']
+var arr22 = ['expand', 'disagree', 'disease', 'hazard']
+var arr23 = ['advice', 'thunder', 'electric', 'helmet']
+var arr24 = ['help', 'west', 'crush', 'music']
 
-// for (var a = 0; a < arr01.length; a++) {
-//   for (var b = 0; b < arr02.length; b++) {
-//     for (var c = 0; c < arr03.length; c++) {
-//       for (var d = 0; d < arr04.length; d++) {
-//         for (var e = 0; e < arr05.length; e++) {
-//           for (var f = 0; f < arr06.length; f++) {
-//             for (var g = 0; g < arr07.length; g++) {
-//               for (var h = 0; h < arr08.length; h++) {
-//                 for (var i = 0; i < arr09.length; i++) {
-//                   for (var j = 0; j < arr10.length; j++) {
-//                     for (var k = 0; k < arr11.length; k++) {
-//                       for (var l = 0; l < arr12.length; l++) {
-//                         for (var m = 0; m < arr13.length; m++) {
-//                           for (var n = 0; n < arr14.length; n++) {
-//                             for (var o = 0; o < arr15.length; o++) {
-//                               for (var p = 0; p < arr16.length; p++) {
-//                                 for (var q = 0; q < arr17.length; q++) {
-//                                   for (var r = 0; r < arr18.length; r++) {
-//                                     for (var s = 0; s < arr19.length; s++) {
-//                                       for (var t = 0; t < arr20.length; t++) {
-//                                         for (var u = 0; u < arr21.length; u++) {
-//                                           for (var v = 0; v < arr22.length; v++) {
-//                                             for (var w = 0; w < arr23.length; w++) {
-//                                               for (var x = 0; x < arr24.length; x++) {
+for (var a = 0; a < arr01.length; a++) {
+  for (var b = 0; b < arr02.length; b++) {
+    for (var c = 0; c < arr03.length; c++) {
+      for (var d = 0; d < arr04.length; d++) {
+        for (var e = 0; e < arr05.length; e++) {
+          for (var f = 0; f < arr06.length; f++) {
+            for (var g = 0; g < arr07.length; g++) {
+              for (var h = 0; h < arr08.length; h++) {
+                for (var i = 0; i < arr09.length; i++) {
+                  for (var j = 0; j < arr10.length; j++) {
+                    for (var k = 0; k < arr11.length; k++) {
+                      for (var l = 0; l < arr12.length; l++) {
+                        for (var m = 0; m < arr13.length; m++) {
+                          for (var n = 0; n < arr14.length; n++) {
+                            for (var o = 0; o < arr15.length; o++) {
+                              for (var p = 0; p < arr16.length; p++) {
+                                for (var q = 0; q < arr17.length; q++) {
+                                  for (var r = 0; r < arr18.length; r++) {
+                                    for (var s = 0; s < arr19.length; s++) {
+                                      for (var t = 0; t < arr20.length; t++) {
+                                        for (var u = 0; u < arr21.length; u++) {
+                                          for (var v = 0; v < arr22.length; v++) {
+                                            for (var w = 0; w < arr23.length; w++) {
+                                              for (var x = 0; x < arr24.length; x++) {
 
-//                                                 console.log(
-//                                                   arr01[a] +
-//                                                   arr02[b] +
-//                                                   arr03[c] +
-//                                                   arr04[d] +
-//                                                   arr05[e] +
-//                                                   arr06[f] +
-//                                                   arr07[g] +
-//                                                   arr08[h] +
-//                                                   arr09[i] +
-//                                                   arr10[j] +
-//                                                   arr11[k] +
-//                                                   arr12[l] +
-//                                                   arr13[m] +
-//                                                   arr14[n] +
-//                                                   arr15[o] +
-//                                                   arr16[p] +
-//                                                   arr17[q] +
-//                                                   arr18[r] +
-//                                                   arr19[s] +
-//                                                   arr20[t] +
-//                                                   arr21[u] +
-//                                                   arr22[v] +
-//                                                   arr23[w] +
-//                                                   arr24[x]
-//                                                 )
-//                                               }
-//                                             }
-//                                           }
-//                                         }
-//                                       }
-//                                     }
-//                                   }
-//                                 }
-//                               }
-//                             }
-//                           }
-//                         }
-//                       }
-//                     }
-//                   }
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
+                                                console.log(
+                                                  arr01[a] + " " +
+                                                  arr02[b] + " " +
+                                                  arr03[c] + " " +
+                                                  arr04[d] + " " +
+                                                  arr05[e] + " " +
+                                                  arr06[f] + " " +
+                                                  arr07[g] + " " +
+                                                  arr08[h] + " " +
+                                                  arr09[i] + " " +
+                                                  arr10[j] + " " +
+                                                  arr11[k] + " " +
+                                                  arr12[l] + " " +
+                                                  arr13[m] + " " +
+                                                  arr14[n] + " " +
+                                                  arr15[o] + " " +
+                                                  arr16[p] + " " +
+                                                  arr17[q] + " " +
+                                                  arr18[r] + " " +
+                                                  arr19[s] + " " +
+                                                  arr20[t] + " " +
+                                                  arr21[u] + " " +
+                                                  arr22[v] + " " +
+                                                  arr23[w] + " " +
+                                                  arr24[x]
+                                                )
+                                              }
+                                            }
+                                          }
+                                        }
+                                      }
+                                    }
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
 
-//   }
+  }
 
-// }
-// console.log(output)
+}
+
+var end = new Date().getTime()
+var math = start - end
+console.log((math * .001).toFixed(3) + " seconds")
